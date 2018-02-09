b8fb3f9f24dde9563d1d52f73dad754baf06155f
semodi <sebastian-dick@gmx.net>
2018-02-08 20:53:47 -0500

    can now run propagate()

--- pimd_wrapper.py
+++ pimd_wrapper.py
@@ -15,10 +15,11 @@ import time
 import hashlib
 import shutil
 import os
+import subprocess
 eVtokcal = 23.06035
 kcaltoeV = 1/eVtokcal
 
-
+#TODO: also intialize velocities (or make it an option), read coordinates/veloctities and return them 
 class PIMDPropagator:
       
     def __init__(self, atoms, steps=10,
@@ -41,18 +42,24 @@ class PIMDPropagator:
         self._update_input_file()         
         self._update_xyz_file()
         
-#    def __del__(self):
-#        os.remove(self.filename + '.inp')
-#        os.remove(self.filename + '.xyz')
+    def __del__(self):
+        os.remove(self.filename + '.inp')
+        os.remove(self.filename + '.xyz')
 
     def _update_xyz_file(self):
         write(self.filename + '.xyz', self.atoms)
-
+        with open(self.filename + '.xyz', 'r+') as file:
+            n_atoms = file.readline()
+            sidelength = file.readline().split()[4]
+            file.seek(0)
+            file.write('{} \t {} \t {} \n'.format(*([sidelength]*3)))
+            file.write(n_atoms)
+            
     def _update_input_file(self):
         with open('.pimd_input.template','r') as inputfile:
             input_template = inputfile.read()
-            replace_dict = {'positions_file': self.filename + '.xyz',
-                            'name': self.filename,
+            replace_dict = {'positions_file': "'" + self.filename + ".xyz'",
+                            'name': "'" + self.filename + "'",
                             'num_steps': self.steps,
                             'time_step': self.dt,
                             'output_freq': self.output_freq,
@@ -79,12 +86,15 @@ class PIMDPropagator:
 
     def set_atoms(self, atoms):
         self.atoms = atoms
+        self._update_xyz_file()
 
     def get_atoms(self, atoms):
         return self.atoms 
 
     def propagate(self):
-        pass            
+        cmd = ['PIMD.x ' + self.filename +'.inp']
+        subprocess.check_call(cmd, shell = True)
+                
 
     def get_stress(self, atoms):
         return np.zeros([3,3])
@@ -95,4 +105,4 @@ if __name__ == '__main__':
     h2o.set_cell([[15.646,0,0],[0,15.646,0],[0,0,15.646]])
     h2o.set_pbc(True)
     prop = PIMDPropagator(h2o)
-
+    prop.propagate()
