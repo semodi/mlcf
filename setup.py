68ecb792f2f9545ee8f0ad0f3bea7e7a6bd7cdd5
semodi <sebastian.dick@stonybrook.edu>
2018-04-25 11:36:28 -0400

    siestah2o module and parallel file io

---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='siestah2o',
+        version="0.1.1",
+        description='',
+        author='Sebastian Dick',
+        author_email='sebastian.dick@stonybrook.edu',
+        url="",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
