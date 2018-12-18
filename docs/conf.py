b268687f880e993dd45d999268cdf2a8cd700a94
semodi <sebastian-dick@gmx.net>
2018-12-18 15:26:00 -0500

    first module documentation

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'MLCF'
+project = 'mlc_func'
 copyright = '2018, Sebastian Dick'
 author = 'Sebastian Dick'
 
@@ -41,7 +41,6 @@ release = ''
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -104,7 +103,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MLCFdoc'
+htmlhelp_basename = 'mlc_funcdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -131,7 +130,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'MLCF.tex', 'MLCF Documentation',
+    (master_doc, 'mlc_func.tex', 'mlc\\_func Documentation',
      'Sebastian Dick', 'manual'),
 ]
 
@@ -141,7 +140,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'mlcf', 'MLCF Documentation',
+    (master_doc, 'mlc_func', 'mlc_func Documentation',
      [author], 1)
 ]
 
@@ -152,8 +151,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'MLCF', 'MLCF Documentation',
-     author, 'MLCF', 'One line description of project.',
+    (master_doc, 'mlc_func', 'mlc_func Documentation',
+     author, 'mlc_func', 'One line description of project.',
      'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,10 @@
-.. MLCF documentation master file, created by
-   sphinx-quickstart on Tue Dec 18 12:24:11 2018.
+.. mlc_func documentation master file, created by
+   sphinx-quickstart on Tue Dec 18 12:49:12 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to MLCF's documentation!
-================================
+Welcome to mlc_func's documentation!
+====================================
 
 .. toctree::
    :maxdepth: 2
