ceb56e246447d7d0ec81c9c0c32b8a845d556270
Sebastian Dick <sebastian.dick@stonybrook.edu>
2019-01-25 13:35:54 -0500

    definitely fixed listintegrator

--- mlc_func/md/listintegrator.py
+++ mlc_func/md/listintegrator.py
@@ -14,7 +14,7 @@ def single_thread(atoms_list, calcfile, env):
     os.environ['SIESTA_COMMAND'] = env['SIESTA_COMMAND']
     os.environ['SIESTA_PP_PATH'] = env['SIESTA_PP_PATH']
     os.environ['SIESTA_WORKING_DIR'] = env['SIESTA_WORKING_DIR']
-    #os.chdir(env['SIESTA_WORKING_DIR'])
+    os.chdir(env['SIESTA_WORKING_DIR'])
     calculator = load_from_file(calcfile)
     results = []
 
@@ -55,6 +55,7 @@ class ListIntegrator():
         else:
             self.view = serial_view()
 
+        print(len(client[:]))
         atoms_indexed = [{i: a} for i, a in enumerate(atoms)]
         print(atoms_indexed)
         n_workers = len(self.view)
@@ -65,7 +66,9 @@ class ListIntegrator():
         """
         Run the ListIntegrator until all configurations are calculated
         """
+        print('running')
         env = dict(os.environ)
         self.view.map_sync(single_thread, self.atoms_threaded,
                 [self.calcfile]*len(self.atoms_threaded),
                 [env]*len(self.atoms_threaded))
+        print('done')
